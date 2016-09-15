9e70388bb4ef07079b822a9f77d373accb54271e
austinkwok <austinkwok615@gmail.com>
2016-09-14 18:44:07 -0700

    add session controller

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :user do |f|
     f.first_name { Faker::Name.first_name }
     f.last_name { Faker::Name.last_name }
+    f.sequence(:email) { |n| "#{n}-p@example.com" }
+    f.password { "foobar" } 
+    f.password_confirmation { "foobar" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,38 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   	it "has a valid factory" do
-  		@user = FactoryGirl.create(:user)
-    	expect(@user).to be_valid
+  		user = FactoryGirl.create(:user)
+    	expect(user).to be_valid
   	end
+
   	it "is invalid without a firstname" do
-  		@user = FactoryGirl.build(:user, first_name: " ")
-    	expect(@user).to_not be_valid
+  		user = FactoryGirl.build(:user, first_name: " ")
+    	expect(user).to_not be_valid
   	end
+
   	it "is invalid without a lastname" do 
-  		@user = FactoryGirl.build(:user, last_name: " ")
-  		expect(@user).to_not be_valid
+  		user = FactoryGirl.build(:user, last_name: " ")
+  		expect(user).to_not be_valid
   	end
+
+  	it "has unique email address" do
+  		user = FactoryGirl.create(:user)
+  		duplicate_user = user.dup
+  		duplicate_user.email = user.email.upcase
+  		user.save
+  		expect(duplicate_user).to_not be_valid
+  	end
+
+  	it "should have a present password (nonblank)" do
+  		user = FactoryGirl.build(:user)
+  		user.password = user.password_confirmation = " " * 6
+  		expect(user).to_not be_valid
+  	end
+
+  	it "should have a minimum length for password" do
+  		user = FactoryGirl.build(:user)
+  		user.password = user.password_confirmation = "a" * 5
+  		expect(user).to_not be_valid
+  	end
+
 end
